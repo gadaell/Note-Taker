9243f783d3fb319a2af6131c787374445681a3e5
A'Darius Ellis <85079903+gadaell@users.noreply.github.com>
2021-09-12 09:07:55 -0500

    fixed error in htmlroutes.js changes syntax from '*' to '/'

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -16,7 +16,7 @@ router.get("/api/notes", (req, res) => {
 router.post("/api/notes", (req, res) => {
   // Read data from 'db.json' file
   let data = JSON.parse(fs.readFileSync("./db/db.json"));
-  const newNote = request.body;
+  const newNote = req.body;
   // Assigned unique id obtained from 'uuid' package
   newNote.id = uuidv4();
   // Extracted new note from request body, and push to db file
@@ -28,6 +28,27 @@ router.post("/api/notes", (req, res) => {
   console.log("\nSuccessfully added new note to 'db.json' file!");
   // Send response
   res.json(data);
+});
+// API DELETE request
+router.delete("/api/notes/:id", (req, res) => {
+  // Fetched id to delete
+  let noteId = req.params.id.toString();
+
+  console.log(`\n\nDELETE note request for noteId: ${noteId}`);
+
+  // Read data from 'db.json' file
+  let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+  // filter data to get notes except the one to delete
+  const newInfo = data.filter((note) => note.id.toString() !== noteId);
+
+  // Write new data to 'db.json' file
+  fs.writeFileSync("./db/db.json", JSON.stringify(newInfo));
+
+  console.log(`\nSuccessfully deleted note with id : ${noteId}`);
+
+  // Send response
+  res.json(newInfo);
 });
 
 module.exports = router;
